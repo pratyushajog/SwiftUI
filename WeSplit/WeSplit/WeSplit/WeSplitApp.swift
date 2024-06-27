129c1fe043a99617561285730efcb3c277838716
pratyushajog <65998064+pratyushajog@users.noreply.github.com>
2024-06-27 14:30:48 -0700

    Initial setup of WeSplit

---
+++ WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Pratyusha Joginipally on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
